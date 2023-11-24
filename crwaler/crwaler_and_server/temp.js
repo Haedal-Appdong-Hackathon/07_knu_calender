78bc17e1860be4f218c456347449cd1f89e8268d
kdongsu5509 <kdongsu5509@gmail.com>
2023-11-25 04:31:05 +0900

    duplicate prevention

--- crwaler/crwaler_and_server/temp.js
+++ crwaler/crwaler_and_server/temp.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 const fs = require('fs');
+const fsp = require('node:fs/promises');
 const path = require('path');
 const cron = require('node-cron');
 const mongoose = require('mongoose');
@@ -11,24 +12,6 @@ const { spawn } = require('child_process');
 
 const PORT = 3000;
 const databaseURL = "mongodb+srv://dongsu108:dssmongoose76025509@crawling.9qzybah.mongodb.net/?retryWrites=true&w=majority";
-
-// function writeFile(name, msg){
-//     if(name == "") return false;	
-//     var defaultpath = "C:"; // 기록하고자 하는 경로. ex) C:\\Program Files\\logs	
-//     var fileObject = new ActiveXObject("Scripting.FileSystemObject");	
-//     var fullpath = defaultpath+"\\"+name; 
-//     // 파일이 생성되어있지 않으면 새로 만들고 기록	
-//     if(!fileObject.FileExists(fullpath)){
-//         var fWrite = fileObject.CreateTextFile(fullpath,false);		
-//         fWrite.write(msg);		
-//         fWrite.close();
-//     }else
-//     {		
-//         var fWrite = fileObject.OpenTextFile(fullpath, 8);		
-//         fWrite.write(msg);		
-//         fWrite.close();	
-//     }
-// }
 
 mongoose.connect(databaseURL)
     .then(() => {
@@ -60,59 +43,101 @@ mongoose.connect(databaseURL)
         }
 
         // cron 작업: 매일 새벽 2시에 실행
-// ...
+        // cron 작업: 매일 새벽 2시에 실행
+        cron.schedule('26 4 * * *', async () => {
+            console.log('Cron job이 시작되었습니다.'); // 추가된 라인
 
-        cron.schedule('21 0 * * *', async () => {
             try {
                 const directoryPath = path.join(__dirname, '..', 'storage');
                 const files = fs.readdirSync(directoryPath);
 
+                console.log('파일 목록:', files); // 추가된 라인
+
                 for (const file of files) {
                     const filePath = path.join(directoryPath, file);
+                    console.log('파일 경로:', filePath); // 추가된 라인
+
                     const jsonData = fs.readFileSync(filePath, 'utf-8');
+                    console.log('파일 내용:', jsonData); // 추가된 라인
+
                     const jsonObjects = jsonData.split('\n').filter(Boolean);
 
                     for (const jsonString of jsonObjects) {
+                        console.log('JSON 문자열:', jsonString); // 추가된 라인
+
                         try {
                             const jsonObject = JSON.parse(jsonString);
-                            //
+                            console.log('파싱된 JSON 객체:', jsonObject); // 추가된 라인
+
                             if (await dbCheck(jsonObject, DataModel)) {
                                 await DataModel.create(jsonObject);
+
+                                const log = jsonObject.link;
+                                const nameOfFile = "sendedFileList";
+
+                                console.log(directoryPath);
+
+                                // 파일에 ID가 있는지 확인하고, 없으면 추가
+                                async function processAndWriteToFile(log, directoryPath, nameOfFile) {
+                                    const filePosition = path.join(directoryPath, nameOfFile);
+
+                                    try {
+                                        // 파일이 존재하지 않으면 새로 생성
+                                        await fsp.access(filePosition, fs.constants.F_OK)
+                                            .then(() => console.log('파일이 이미 존재합니다.'))
+                                            .catch(async () => {
+                                                console.log('파일이 존재하지 않습니다. 새로 생성합니다.');
+                                                await fsp.writeFile(filePosition, '');
+                                            });
+
+                                        const fileContent = await fsp.readFile(filePosition, 'utf-8');
+
+                                        // 파일에 ID가 이미 있는 경우
+                                        if (fileContent.includes(log)) {
+                                            console.log(`ID(${log})가 이미 파일에 존재합니다.`);
+                                        } else {
+                                            // 파일에 ID가 없는 경우
+                                            console.log(`ID(${log})를 파일에 추가합니다.`);
+                                            await fsp.appendFile(filePosition, `${log}\n`);
+                                        }
+                                    } catch (error) {
+                                        console.error(`파일 처리 중 오류 발생: ${error.message}`);
+                                    }
+                                }
+
+                                // 서버로 데이터 전송
+                                async function sendDataToServer(fe) {
+                                    try {
+                                        // 서버로 데이터 전송하는 코드
+                                        await axios.post('http://3.38.190.145:8080/knu/receive', fe, { timeout: 5000 });
+                                        console.log('데이터를 서버로 성공적으로 전송했습니다');
+                                    } catch (error) {
+                                        console.error('POST request error:', error);
+                                    }
+                                }
+
+                                // 파일에 ID 추가
+                                await processAndWriteToFile(log, directoryPath, nameOfFile); // 여기서 막혀서 더 이상 안되는 듯.
+
+                                // 서버로 데이터 전송
+                                const fe = {
+                                    title: jsonObject.title,
+                                    category: jsonObject.category,
+                                    date: jsonObject.date,
+                                    link: jsonObject.link,
+                                };
+                                await sendDataToServer(fe);
                             }
                         } catch (error) {
                             console.error(`Error parsing JSON: ${error.message}`);
                         }
                     }
                 }
-
-                const dataFromDB = await DataModel.find();
-                const dataToSend = dataFromDB.map(({ _id, __v, ...rest }) => rest);
-                console.log(dataToSend);
-
-                try {
-                    for (const se of dataToSend) {
-                        const fe = {
-                            title: se._doc.title,
-                            category: se._doc.category,
-                            date: se._doc.date,
-                            link: se._doc.link,
-                        };
-                        // writeFile(upload.js, fe);
-                        console.log(fe);
-                        const response = await axios.post('http://3.38.250.63:8080/knu/receive', fe);
-                        console.log(response.data);
-                        console.log('데이터를 서버로 성공적으로 전송했습니다');
-                    }
-                } catch (error) {
-                    console.error('POST request error:', error);
-                }
-
             } catch (error) {
                 console.error('오류 발생:', error);
             }
+            console.log('Cron job이 종료되었습니다.'); // 추가된 라인
         });
-
-        // ...
 
 
         // Express 서버 시작
@@ -126,7 +151,8 @@ mongoose.connect(databaseURL)
         console.error('MongoDB 연결 중 오류 발생:', error);
     });
 
+// MongoDB에 데이터가 있는지 확인
 async function dbCheck(data, DataModel) {
-    const existingData = await DataModel.findOne({ title: data.title, date:data.date});
+    const existingData = await DataModel.findOne({ title: data.title, date: data.date });
     return existingData == null;
 }
